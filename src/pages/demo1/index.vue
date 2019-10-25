2ca342b603299cbae792a9164f696838f2a11b69
Xianda Wu <31464273+YintatNg@users.noreply.github.com>
2019-10-25 14:31:22 +0800

    a

---
+++ src/pages/demo1/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>helloworld</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  }
+};
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,21 @@
 import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import demo1 from "@/pages/demo1/index";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "HelloWorld",
+      component: HelloWorld
+    },
+    {
+      path: "/demo1",
+      name: "demo1",
+      component: demo1
+    }
+  ]
+});
